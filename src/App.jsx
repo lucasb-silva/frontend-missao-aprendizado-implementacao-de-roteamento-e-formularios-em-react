96a2b152d9fe84b15f9f9e8cdcf1bbce05d7e3b4
Lucas Silva <59655760+lucasb-silva@users.noreply.github.com>
2024-10-03 21:34:11 +0000

    Criação da página `<Home />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,23 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import Card from './components/Card/Card'
-import { toast, ToastContainer } from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import { Api } from './api/api'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
 
-function App() {
-
-  const [devmons, setDevmons] = useState([])
-
-  async function fetchData() {
-    const apiURL = Api.personagem.readAll()
-
-    const response = await Api.buildApiGetRequest(apiURL)
-
-    if (response.ok) {
-      const data = await response.json()
-
-      setDevmons(data)
-    } else {
-      toast.error('Erro ao carregar lista de Devmon')
-    }
-
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
   }
+])
 
-  useEffect(function () {
-    fetchData()
-  }, [])
+export default function App() {
 
   return (
     <>
-      Lista de Cards com Map: <br />
-      <div className="cards">
-        {devmons.map(function (devmon) {
-          return <Card key={devmon.nome} item={devmon} />
-        })}
-      </div >
+      <RouterProvider router={router} />
       <ToastContainer />
     </>
   )
 }
 
-export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import { Api } from '../api/api'
+import Card from '../components/Card/Card'
+
+export default function Home() {
+  const [devmons, setDevmons] = useState([])
+
+  async function fetchData() {
+    const apiURL = Api.personagem.readAll()
+
+    const response = await Api.buildApiGetRequest(apiURL)
+
+    if (response.ok) {
+      const data = await response.json()
+
+      setDevmons(data)
+    } else {
+      toast.error('Erro ao carregar lista de Devmon')
+    }
+
+  }
+
+  useEffect(function () {
+    fetchData()
+  }, [])
+
+  return (
+    <>
+      Lista de Cards com Map: <br />
+      <div className="cards">
+        {devmons.map(function (devmon) {
+          return <Card key={devmon.nome} item={devmon} />
+        })}
+      </div >
+    </>
+  )
+}
