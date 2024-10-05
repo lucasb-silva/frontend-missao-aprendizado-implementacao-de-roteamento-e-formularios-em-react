fca5b82e0b5db3e75e1b5856dadbb8d4976ca6d8
Lucas Silva <59655760+lucasb-silva@users.noreply.github.com>
2024-10-05 02:46:17 +0000

    API implementada na página de `Create`

--- src/api/api.js
+++ src/api/api.js
@@ -7,14 +7,30 @@ export const Api = {
     },
     readAll: function () {
       return this.endpoint() + '/'
+    },
+    create: function () {
+      return this.endpoint() + '/'
     }
   },
-  
+
   buildApiGetRequest: function (url) {
     return fetch(url, { method: 'GET' }).catch(function (error) {
-      console.error('Erro ao carssregar dados ' + url, error)
+      console.error('Erro ao carregar dados ' + url, error)
       toast.error('Erro ao carregar dados')
     })
+  },
+  buildApiPostRequest: function (url, body) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    })
+      .catch(function (error) {
+        console.error('Erro ao enviar dados ' + url, error)
+        toast.error('Erro ao enviar dados')
+      })
   }
 
 
